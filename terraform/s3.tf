daf5f7a06e87fe1f794f5e3965dac05fd855f5b7
Cullan Carey <cullancarey@Not-your-Mac-mini.local>
2022-10-26 10:27:15 -0400

    adding s3 bucket policy for lambda user

---
+++ terraform/s3.tf
@@ -0,0 +1,35 @@
+#############################################
+######### YOUTUBE UPLOADER BUCKET ###########
+#############################################
+
+
+resource "aws_s3_bucket" "youtube_uploader_bucket" {
+  bucket              = "youtube-uploader-bucket"
+  tags = {
+    "Name" = "youtube-uploader-bucket"
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "allow_access_from_lambda_user" {
+  bucket = aws_s3_bucket.youtube_uploader_bucket.id
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowLambda",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::045107234435:role/service-role/youtube_video_generator-role"
+            },
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket"
+            ],
+            "Resource": "arn:aws:s3:::youtube-uploader-bucket/*"
+        }
+    ]
+}
+POLICY
+}
