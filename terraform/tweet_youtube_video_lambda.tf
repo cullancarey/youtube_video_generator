380a4b151e48904536810f039bc7713f0ee324a3
Cullan Carey <cullancarey@gmail.com>
2025-04-12 11:08:27 -0400

    Update function name to use local variable for tweet Lambda and add description [tf-plan]

--- terraform/tweet_youtube_video_lambda.tf
+++ terraform/tweet_youtube_video_lambda.tf
@@ -10,7 +10,8 @@
 # }
 
 resource "aws_lambda_function" "tweet_youtube_video_lambda" {
-  function_name = "tweet_lambda"
+  function_name = local.tweet_video_lambda
+  description   = "Lambda function for tweeting a youtube video"
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.tweet_lambda.repository_url}:latest"
   role          = aws_iam_role.iam_for_tweet_youtube_video_lambda.arn
