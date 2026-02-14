14456f3299c3483f5dc1a6f63a7ee0873e52b666
Cullan Carey <cullancarey@gmail.com>
2026-02-14 06:31:22 -0500

    Fix formatting in main.tf for AWS provider default tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,8 @@ provider "aws" {
   region = "us-east-2"
   default_tags {
     tags = {
-      Project = "youtube_video_generator"
+      Project   = "youtube_video_generator"
+      Terraform = "true"
     }
   }
 }
